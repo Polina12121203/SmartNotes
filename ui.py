4436db08a15999b05111e52e1dd8fb9cbc07f59c
Polina <polina008r@gmail.com>
2025-02-01 17:33:44 +0200

    Add files via upload

--- ui.py
+++ ui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'photo.ui'
+# Form implementation generated from reading ui file 'file1111.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.11
 #
@@ -14,56 +14,74 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 559)
-        MainWindow.setStyleSheet("background-color: rgb(250, 255, 255);\n"
-"background-color: rgb(253, 255, 255);")
+        MainWindow.resize(800, 623)
+        MainWindow.setStyleSheet("background-color: rgb(255, 255, 230);")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(190, 20, 591, 451))
-        self.label.setStyleSheet("background-color: rgb(242, 255, 255);\n"
-"font: 63 10pt \"Yu Gothic UI Semibold\";")
-        self.label.setObjectName("label")
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit.setGeometry(QtCore.QRect(10, 10, 461, 571))
+        self.textEdit.setStyleSheet("background-color: rgb(242, 242, 242);")
+        self.textEdit.setObjectName("textEdit")
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(10, 20, 161, 31))
-        self.pushButton.setStyleSheet("background-color: rgb(242, 255, 255);\n"
-"color: rgb(0, 56, 62);\n"
-"font: 63 10pt \"Yu Gothic UI Semibold\";")
+        self.pushButton.setGeometry(QtCore.QRect(480, 250, 311, 23))
+        self.pushButton.setStyleSheet("background-color: rgb(242, 242, 242);")
         self.pushButton.setObjectName("pushButton")
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(190, 490, 111, 23))
-        self.pushButton_2.setStyleSheet("background-color: rgb(242, 255, 255);\n"
-"font: 63 10pt \"Yu Gothic UI Semibold\";\n"
-"color: rgb(0, 56, 62);")
+        self.pushButton_2.setGeometry(QtCore.QRect(640, 220, 151, 23))
+        self.pushButton_2.setStyleSheet("background-color: rgb(242, 242, 242);")
         self.pushButton_2.setObjectName("pushButton_2")
-        self.listWidget = QtWidgets.QListWidget(self.centralwidget)
-        self.listWidget.setGeometry(QtCore.QRect(10, 60, 161, 451))
-        self.listWidget.setStyleSheet("background-color: rgb(242, 255, 255);")
-        self.listWidget.setObjectName("listWidget")
         self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_3.setGeometry(QtCore.QRect(430, 490, 111, 23))
-        self.pushButton_3.setStyleSheet("background-color: rgb(242, 255, 255);\n"
-"font: 63 10pt \"Yu Gothic UI Semibold\";\n"
-"color: rgb(0, 56, 62);")
+        self.pushButton_3.setGeometry(QtCore.QRect(480, 220, 151, 23))
+        self.pushButton_3.setStyleSheet("background-color: rgb(242, 242, 242);\n"
+"font: 75 8pt \"MS Shell Dlg 2\";")
         self.pushButton_3.setObjectName("pushButton_3")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(480, 6, 311, 20))
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(11)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(9)
+        self.label.setFont(font)
+        self.label.setStyleSheet("font: 75 11pt \"Calibri\";")
+        self.label.setObjectName("label")
+        self.listWidget = QtWidgets.QListWidget(self.centralwidget)
+        self.listWidget.setGeometry(QtCore.QRect(480, 30, 311, 181))
+        self.listWidget.setStyleSheet("background-color: rgb(242, 242, 242);")
+        self.listWidget.setObjectName("listWidget")
+        self.listWidget_2 = QtWidgets.QListWidget(self.centralwidget)
+        self.listWidget_2.setGeometry(QtCore.QRect(480, 310, 311, 161))
+        self.listWidget_2.setStyleSheet("background-color: rgb(242, 242, 242);")
+        self.listWidget_2.setObjectName("listWidget_2")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(480, 286, 311, 20))
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(11)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(50)
+        self.label_2.setFont(font)
+        self.label_2.setStyleSheet("font: 75 8pt \"MS Shell Dlg 2\";\n"
+"font: 11pt \"Calibri\";")
+        self.label_2.setObjectName("label_2")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(480, 480, 311, 20))
+        self.lineEdit.setStyleSheet("background-color: rgb(242, 242, 242);")
+        self.lineEdit.setObjectName("lineEdit")
         self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_4.setGeometry(QtCore.QRect(310, 490, 111, 23))
-        self.pushButton_4.setStyleSheet("background-color: rgb(242, 255, 255);\n"
-"font: 63 10pt \"Yu Gothic UI Semibold\";\n"
-"color: rgb(0, 56, 62);")
+        self.pushButton_4.setGeometry(QtCore.QRect(480, 510, 151, 23))
+        self.pushButton_4.setStyleSheet("background-color: rgb(242, 242, 242);")
         self.pushButton_4.setObjectName("pushButton_4")
+        self.pushButton_5 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_5.setGeometry(QtCore.QRect(640, 510, 151, 23))
+        self.pushButton_5.setStyleSheet("background-color: rgb(242, 242, 242);")
+        self.pushButton_5.setObjectName("pushButton_5")
         self.pushButton_6 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_6.setGeometry(QtCore.QRect(550, 490, 111, 23))
-        self.pushButton_6.setStyleSheet("background-color: rgb(242, 255, 255);\n"
-"font: 63 10pt \"Yu Gothic UI Semibold\";\n"
-"color: rgb(0, 56, 62);")
+        self.pushButton_6.setGeometry(QtCore.QRect(480, 540, 311, 23))
+        self.pushButton_6.setStyleSheet("background-color: rgb(242, 242, 242);")
         self.pushButton_6.setObjectName("pushButton_6")
-        self.pushButton_7 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_7.setGeometry(QtCore.QRect(670, 490, 111, 23))
-        self.pushButton_7.setStyleSheet("background-color: rgb(242, 255, 255);\n"
-"font: 63 10pt \"Yu Gothic UI Semibold\";\n"
-"color: rgb(0, 56, 62);")
-        self.pushButton_7.setObjectName("pushButton_7")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
@@ -79,13 +97,14 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setText(_translate("MainWindow", "картинка"))
-        self.pushButton.setText(_translate("MainWindow", "Папка"))
-        self.pushButton_2.setText(_translate("MainWindow", "Вліво"))
-        self.pushButton_3.setText(_translate("MainWindow", "Дзеркало"))
-        self.pushButton_4.setText(_translate("MainWindow", "Вправо"))
-        self.pushButton_6.setText(_translate("MainWindow", "Різкість"))
-        self.pushButton_7.setText(_translate("MainWindow", "Ч/Б"))
+        self.pushButton.setText(_translate("MainWindow", "Зберегти замітку"))
+        self.pushButton_2.setText(_translate("MainWindow", "Видалити замітку"))
+        self.pushButton_3.setText(_translate("MainWindow", "Створити замітку"))
+        self.label.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-weight:600;\">Список заміток</span></p></body></html>"))
+        self.label_2.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-weight:600;\">Список тегів</span></p></body></html>"))
+        self.pushButton_4.setText(_translate("MainWindow", "Додати до замітки"))
+        self.pushButton_5.setText(_translate("MainWindow", "Відкріпити від замітки"))
+        self.pushButton_6.setText(_translate("MainWindow", "Шукати замітку по тегу"))
 
 
 if __name__ == "__main__":
